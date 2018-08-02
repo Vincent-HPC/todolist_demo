9dfbfd0bbc26ac00ecd2e3bcbd0beee8b2350c45
Vincent <pchuang92738@gmail.com>
2018-08-02 14:08:42 +0800

    Debug for double detect blur between new & update

--- js/action.js
+++ js/action.js
@@ -4,38 +4,33 @@ $(document).ready(function () {
   var source = $('#todo-list-item-template').html();
   var todoTemplate = Handlebars.compile(source);
 
-  // create
-  $('li.new').find('.content').blur(function (e) {
-    var todo = $(e.currentTarget).text();
-    todo = todo.trim();
-
-    if (todo.length > 0) {
-      todo = {
-        is_complete: false,
-        content: todo,
-      };
-      var li = todoTemplate(todo);
-      $(e.currentTarget).closest('li').before(li);
-    }
-    // var context = {title: "My New Post", body: "This is my first post!"};
-    // var html    = template(context);
-
-    // clear new todo item
-    $(e.currentTarget).empty();
-  });
-
   // enter editor mode
   $('#todo-list')
     .on('dblclick', '.content', function (e) {
       $(this).prop('contenteditable', true).focus();
     })
     .on('blur', '.content', function (e) {
-      // var isNew = $(this).closest('li').is('.new');
-      // console.log(isNew);
+      var isNew = $(this).closest('li').is('.new');
+      if (isNew) {
+        var todo = $(e.currentTarget).text();
+        todo = todo.trim();
 
-      $(this).prop('contenteditable', false);
+        if (todo.length > 0) {
+          todo = {
+            is_complete: false,
+            content: todo,
+          };
+          var li = todoTemplate(todo);
+          $(e.currentTarget).closest('li').before(li);
+        }
+        // var context = {title: "My New Post", body: "This is my first post!"};
+        // var html    = template(context);
+
+        // clear new todo item
+        $(e.currentTarget).empty();
+      } else {
+        $(this).prop('contenteditable', false);
+      }
     });
-
-
 
 });
