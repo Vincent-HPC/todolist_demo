533001b51443d7fb2c671b0acc4ff9b4d935505b
Vincent <pchuang92738@gmail.com>
2018-08-02 13:53:29 +0800

    Init commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php include('header.php') ?>
+
+<div id="panel">
+  <h1>Todo List</h1>
+
+  <div id="todo-list">
+    <ul>
+      <li>
+        <div class="checkbox"></div>
+        <div class="content">Lorem, ipsum dolor.</div>
+        <div class="actions">
+          <div class="delete">x</div>
+        </div>
+      </li>
+      <li class="complete">
+        <div class="checkbox"></div>
+        <div class="content">Lorem, ipsum dolor.</div>
+        <div class="actions">
+          <div class="delete">x</div>
+        </div>
+      </li>
+      <li>
+        <div class="checkbox"></div>
+        <div class="content">Lorem, ipsum dolor.</div>
+        <div class="actions">
+          <div class="delete">x</div>
+        </div>
+        <!-- clear: both -->
+      </li>
+      <li class="new">
+        <div class="checkbox"></div>
+        <div class="content" contenteditable="true"></div>
+      </li>
+    </ul>
+  </div>
+</div>
+
+<!-- <div class="template hide">
+  <li class="complete">
+    <div class="checkbox"></div>
+    <div class="content"></div>
+    <div class="actions">
+      <div class="delete">x</div>
+    </div>
+  </li>
+</div>  -->
+
+<script id= "todo-list-item-template" type="text/x-handlebars-template">
+  <li class="{{#if is_complete}}complete{{/if}}">
+    <div class="checkbox"></div>
+    <div class="content">{{content}}</div>
+    <div class="actions">
+      <div class="delete">x</div>
+    </div>
+  </li>
+</script>
+
+  
+<?php include('footer.php') ?>
---
+++ js/action.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+  // todo list item structure,it's fix template
+  var source = $('#todo-list-item-template').html();
+  var todoTemplate = Handlebars.compile(source);
+
+  // create
+  $('li.new').find('.content').blur(function (e) {
+    var todo = $(e.currentTarget).text();
+    todo = todo.trim();
+
+    if (todo.length > 0) {
+      todo = {
+        is_complete: false,
+        content: todo,
+      };
+      var li = todoTemplate(todo);
+      $(e.currentTarget).closest('li').before(li);
+    }
+    // var context = {title: "My New Post", body: "This is my first post!"};
+    // var html    = template(context);
+
+    // clear new todo item
+    $(e.currentTarget).empty();
+  });
+
+  // enter editor mode
+  $('#todo-list')
+    .on('dblclick', '.content', function (e) {
+      $(this).prop('contenteditable', true).focus();
+    })
+    .on('blur', '.content', function (e) {
+      // var isNew = $(this).closest('li').is('.new');
+      // console.log(isNew);
+
+      $(this).prop('contenteditable', false);
+    });
+
+
+
+});
